071546775a52eb32646dcb220299baa4e988657a
Yoav Prat <yoav.prat@workito.co.il>
2020-12-21 10:05:54 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import connectToState from "./connectToState";
 import CCF from "./CreditCardInput";
 import CV from "./CardView";
 
-export const LiteCreditCardInput = connectToState(LiteCCF);
+export const CreditCardInput = connectToState(CCF);
 export const CardView = CV;
