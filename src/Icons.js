d8ce9fe0dc29de3e8abd11501a7cbb1ca75db56d
Yoav Prat <yoav.prat@workito.co.il>
2020-12-21 10:04:28 +0200

    missing stuff

--- src/Icons.js
+++ src/Icons.js
@@ -1,6 +1,5 @@
 const Icons = {
   cvc: require("./icons/stp_card_cvc.png"),
-  cvc_amex: require("./icons/stp_card_cvc_amex.png"),
   "american-express": require("./icons/stp_card_amex.png"),
   "diners-club": require("./icons/stp_card_diners.png"),
   "master-card": require("./icons/stp_card_mastercard.png"),
